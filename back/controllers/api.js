899f4a7831476f87ea822ce7f4391800b747fc63
franktronics <franktronicsprog@gmail.com>
2023-01-18 13:50:33 +0100

    (feat): add access db

--- back/controllers/api.js
+++ back/controllers/api.js
@@ -1,6 +1,20 @@
+const ADODB = require('node-adodb');
 
+const source = "../../access-manager/back/utils/test1.mdb"
+const connection = ADODB.open(`Provider=Microsoft.Jet.OLEDB.4.0;Data Source=${source};Persist Security Info=False;`);
+ 
+ 
 exports.test = async (req, res, next) => {
-    res.status(200).json({
-        data: 'bonjour'
-    })
+    try {
+        const users = await connection.query('SELECT * FROM [user]')
+        const data = JSON.stringify(users, null, 2)
+        res.status(200).json({
+            data: data
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(200).json({
+            data: 'error'
+        })
+    }
 }
